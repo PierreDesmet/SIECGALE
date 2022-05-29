35565ee812442523c1b36a04190df106579b447a
b011ldg <pierre.desmet@generali.com>
2022-05-29 14:17:57 +0000

    Ajout des expected loss values
    Fait :
    - Ajout des pertes oracle, satisfaisante, classe modale et aléatoire

--- ner_pytorch/model.py
+++ ner_pytorch/model.py
@@ -1,18 +1,18 @@
 import torch
-import transformers
 import torch.nn as nn
+from transformers import BertModel
 
 from ner_pytorch.config.params import PARAMS
 
 
-def loss_fn(output, target, mask, num_labels):
+def loss_fn(y_pred, y_true, mask, num_labels):
     lfn = nn.CrossEntropyLoss()
     active_loss = mask.view(-1) == 1
-    active_logits = output.view(-1, num_labels)
+    active_logits = y_pred.view(-1, num_labels)
     active_labels = torch.where(
         active_loss,
-        target.view(-1),
-        torch.tensor(lfn.ignore_index).type_as(target)
+        y_true.view(-1),
+        torch.tensor(lfn.ignore_index).type_as(y_true)
     )
     loss = lfn(active_logits, active_labels)
     return loss
@@ -22,18 +22,13 @@ class EntityModel(nn.Module):
     def __init__(self, num_tag):
         super(EntityModel, self).__init__()
         self.num_tag = num_tag
-        self.bert = transformers.BertModel.from_pretrained(PARAMS.PATHS.MODEL, return_dict=False)
-        self.bert_drop_1 = nn.Dropout(0.3)
-        self.bert_drop_2 = nn.Dropout(0.3)
+        self.bert = BertModel.from_pretrained(PARAMS.PATHS.MODEL, return_dict=False)
+        self.dropout = nn.Dropout(0.3)
         self.out_tag = nn.Linear(768, self.num_tag)
     
     def forward(self, ids, mask, token_type_ids, target_tag):
         o1, _ = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
-
-        bo_tag = self.bert_drop_1(o1)
-
+        bo_tag = self.dropout(o1)
         tag = self.out_tag(bo_tag)
-
         loss = loss_fn(tag, target_tag, mask, self.num_tag)
-
         return tag, loss
--- ner_pytorch/preprocessing.py
+++ ner_pytorch/preprocessing.py
@@ -5,15 +5,18 @@ def process_data(df: pd.DataFrame):
     """
     Transforme le `df` en plusieurs numpy arrays :
     - les phrases
-    - les POS
     - les NER
-    - les LabelEncoders associées aux classes (POS et TAG)
+    - le LabelEncoder associé aux classes (Tag)
     """
     df = df.copy()
     enc_tag = LabelEncoder()
 
     # Le '+1' sert à s'assurer que le modèle pourra distinguer le padding (classe 0)
-    # de la première classe à prédire (classe 1). Abhishek n'y avait pas pensé !
+    # de la première classe à prédire (classe 1). 
+    # Abhishek n'y avait pas pensé ! Cela est confirmé par Lukas Nielsen, quand il demande sur YT :
+    # "Hi Abhishek, great tutorial as allways. One problem though; I was thinking that it might cause 
+    # some issues that you extend the target_tag with 0's for the padding, since 0 is likely assigned 
+    # as the encoded value for an actual tag. Am I mistaken in this?
     df['Tag'] = enc_tag.fit_transform(df.Tag) + 1
 
     sentences = df.groupby('Sentence #')['Word'].apply(list).values
