d48b2fe8e047caf357223ca96001cbb0acdee0d3
b011ldg <pierre.desmet@generali.com>
2022-06-06 12:13:13 +0000

    Ajout de la running loss

--- ner_pytorch/engine.py
+++ ner_pytorch/engine.py
@@ -1,7 +1,9 @@
 import torch
 from tqdm import tqdm
 
+from ner_pytorch.model import loss_fn
 from ner_pytorch.config.params import PARAMS
+
 
 def train_fn(data_loader, model, optimizer, device, scheduler, 
              pbar=None, num_epoch=None):
@@ -9,13 +11,19 @@ def train_fn(data_loader, model, optimizer, device, scheduler,
     model.train()
     final_loss = 0
     for num_batch, batch in enumerate(data_loader):
-        
         if device != 'cpu':
             for k, v in batch.items():
                 batch[k] = v.to(device)
         
         optimizer.zero_grad()
-        _, loss = model(**batch)
+        batch_pred, loss = model(**batch)
+        
+        if num_batch % 10 == 0:
+            with torch.no_grad():
+                batch_loss = loss_fn(batch_pred, batch['target_tag'],
+                                     batch['mask'], num_labels=PARAMS.NUM_LABELS)
+                print(f'Batch #{num_batch} : loss = {batch_loss:.6f}')
+        
         loss.backward()
         torch.nn.utils.clip_grad_norm_(parameters=model.parameters(),
                                        max_norm=PARAMS.MODEL.GRAD_MAX_NORM)
